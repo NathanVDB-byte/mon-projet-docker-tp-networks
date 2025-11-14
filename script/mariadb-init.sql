32b2ef60fbfb5479250b0149e4adb42f7e9ca31c
NathanVDB-byte <nathouille89@gmail.com>
2025-11-14 15:27:32 +0100

    Initial commit of Docker project

--- script/mariadb-init.sql
+++ script/mariadb-init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'utilisateurapp'@'172.31.0.2' IDENTIFIED BY 'apppassword';
-GRANT ALL PRIVILEGES ON bdd.* TO 'utilisateurapp'@'172.31.0.2';
+CREATE USER 'appuser'@'172.31.0.2' IDENTIFIED BY 'apppass';
+GRANT ALL PRIVILEGES ON db.* TO 'appuser'@'172.31.0.2';
 FLUSH PRIVILEGES;
